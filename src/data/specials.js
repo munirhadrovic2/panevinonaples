615cd5454cb19802c6d64231d380fba5ddcc3720
munirhadrovic2 <munir.hadrovic@gmail.com>
2025-10-19 22:00:20 +0200

    Menu update antipasti salad

--- src/data/specials.js
+++ src/data/specials.js
@@ -4,21 +4,21 @@ export const specialsItems = [
         description: "bed of spinach, sun-dried tomato, artichoke, and white wine lemon sauce",
         image: "/images/specials/chilean-sea-bass.jpg",
     },
-    {
-        title: "RAVIOLI STUFFED WITH LOBSTER",
-        description: "chopped tarragon, basil, garlic in lite cream sauce and chopped tomato",
-        image: "/images/specials/ravioli-lobster.jpg",
-    },
+    // {
+    //     title: "RAVIOLI STUFFED WITH LOBSTER",
+    //     description: "chopped tarragon, basil, garlic in lite cream sauce and chopped tomato",
+    //     image: "/images/specials/ravioli-lobster.jpg",
+    // },
     {
         title: "STUFFED EGGPLANT",
         description: "eggplant stuffed with ricotta cheese, spinach, sun-dried tomato, parmigiana, provolone topped with pink sauce",
         image: "/images/specials/stuffed-eggplant.jpg",
     },
-    {
-        title: "FETTUCCINE SAFFRON SAUCE",
-        description: "fettuccine with shrimp, arugula salad and saffron sauce",
-        image: "/images/specials/fettuccine-saffron.jpg",
-    },
+    // {
+    //     title: "FETTUCCINE SAFFRON SAUCE",
+    //     description: "fettuccine with shrimp, arugula salad and saffron sauce",
+    //     image: "/images/specials/fettuccine-saffron.jpg",
+    // },
     {
         title: "OSSOBUCCO",
         description: "a High Veal Shank served with Homemade Gnocchi pasta in a Barolo Wine Sauce with Mushrooms",
@@ -54,11 +54,11 @@ export const specialsItems = [
         description: "Mussels sauteed in a White Wine and Garlic Oil Sauce with Herbs & Spices",
         image: "/images/specials/mussels-luciano.jpg",
     },
-    {
-        title: "PASTA BOLOGNESE",
-        description: "Homemade Meat sauce made of lean ground Veal with sliced Mushrooms and a Touch of Cream served With Homemade Pasta",
-        image: "/images/specials/pasta-bolognese.jpg",
-    },
+    // {
+    //     title: "PASTA BOLOGNESE",
+    //     description: "Homemade Meat sauce made of lean ground Veal with sliced Mushrooms and a Touch of Cream served With Homemade Pasta",
+    //     image: "/images/specials/pasta-bolognese.jpg",
+    // },
     {
         title: "CHICKEN SCARPARIELLO",
         description: "Chunks of boneless Chicken breast, Sweet Italian Sausage and Sliced Potatoes with garlic Oil, sauteed in a Lemon-White sauce",
@@ -69,11 +69,11 @@ export const specialsItems = [
         description: "Homemade Cheese-Stuffed Ravioli served in a Pink Sauce topped with chunks of Lobster Meat",
         image: "/images/specials/ravioli-with-lobster.jpg",
     },
-    {
-        title: "PORTABELLO & ROASTED PEPPERS",
-        description: "Portabello Mushrooms and Bell Roasted Peppers sauteed in a touch of Balsamic vinaigrette served on a bed of mix green salad",
-        image: "/images/specials/portabello.jpg",
-    },
+    // {
+    //     title: "PORTABELLO & ROASTED PEPPERS",
+    //     description: "Portabello Mushrooms and Bell Roasted Peppers sauteed in a touch of Balsamic vinaigrette served on a bed of mix green salad",
+    //     image: "/images/specials/portabello.jpg",
+    // },
     {
         title: "SALMON ORGANETTA",
         description: "Wild Alaskan Salmon with bread-crumbs, grilled and served on a bed of Sauteed Spinach in Garlic-Oil, topped with a touch of Lemon-White Wine sauce",
