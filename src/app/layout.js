a6be34751497e6fc4deefa0af604ef549709a44a
munirhadrovic2 <munir.hadrovic@gmail.com>
2026-06-09 11:20:32 +0200

    Add Google Tag Manager integration to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -88,6 +88,8 @@ export const metadata = {
 
 const playfair = Playfair_Display({ subsets: ['latin'], weight: ['400', '700'] });
 
+const GTM_ID = 'GTM-K5QFHRRW';
+
 export default function RootLayout({ children }) {
     const ldJson = {
         '@context': 'https://schema.org',
@@ -140,7 +142,29 @@ export default function RootLayout({ children }) {
 
     return (
         <html lang="en" className={playfair.className}>
+        <head>
+            {/* Google Tag Manager */}
+            <Script id="gtm-base" strategy="afterInteractive">
+                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');`}
+            </Script>
+            {/* End Google Tag Manager */}
+        </head>
         <body className="font-sans">
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+            <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                height="0"
+                width="0"
+                style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
 
         {/* Google Analytics 4 */}
         <Script
